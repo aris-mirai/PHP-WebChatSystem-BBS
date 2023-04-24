3ba16cab2b449a7c44625bd8b984cb5ecd34c5f1
籘原梓 <102363147+FujiwaraAzusa@users.noreply.github.com>
2023-04-24 23:55:55 +0900

    Update upfile.php

--- upfile.php
+++ upfile.php
@@ -21,11 +21,11 @@ try{
         if(is_uploaded_file($_FILES['file']['tmp_name'])){
 			
 				$filetype = ".".pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
-	        	move_uploaded_file($_FILES['file']['tmp_name'], './receive/'.$count."-".$_FILES['file']['name'].$filetype);
-            	echo "<p>"."./receive/".$count."-".$_FILES['file']['name'].$filetype."として保存されました"."</p>";
+	        	move_uploaded_file($_FILES['file']['tmp_name'], './receive/'.$count."-".$_FILES['file']['name']);
+            	echo "<p>"."./receive/".$count."-".$_FILES['file']['name']."として保存されました"."</p>";
 				echo '<h2>UP完了</h2>';
 				//log
-            	$putting='<p><span style="color:yellow">'.$_COOKIE['username'].'さんが</span><a style="color:yellow" href="'."./receive/".$count.$filetype.'"target="_blank" rel="noopener noreferrer">ファイル</a><span style="color:yellow">をUPしました。'."  ".$todaydate.'</span>'."</p>"."\n";
+            	$putting='<p><span style="color:yellow">'.$_COOKIE['username'].'さんが</span><a style="color:yellow" href="'."./receive/".$count."-".$_FILES['file']['name'].'"target="_blank" rel="noopener noreferrer">ファイル</a><span style="color:yellow">をUPしました。'."  ".$todaydate.'</span>'."</p>"."\n";
             	$count++;
 				$filename = fopen('./date/publiclog.txt',"a");
 				fputs($filename, $putting);
